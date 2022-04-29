2e41ae33cfcf2136ff594e29bc5e66faa623297b
Carlos Villamil <c.villamilc@uniandes.edu.co>
2022-04-28 20:25:14 -0500

    Se agrega servicio get para obtener info cafe, se agrega bootstrap al proyecto, actualizacion de imports de modulos

--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Cafe } from '../cafe';
+import { CafeService } from '../cafe.service';
+
 @Component({
   selector: 'app-cafe-list',
   templateUrl: './cafe-list.component.html',
   styleUrls: ['./cafe-list.component.css']
 })
 export class CafeListComponent implements OnInit {
+  cafes: Array<Cafe> = [];
 
-  constructor() { }
+  constructor(private cafeService: CafeService) { }
+
+  getCafes(): void{
+    this.cafeService.getCafes().subscribe(cafes=>{
+      this.cafes=cafes;
+      console.log(this.cafes);
+    });
+  }
 
   ngOnInit() {
+    this.getCafes();
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+const baseUrl = "https://gist.githubusercontent.com/josejbocanegra/e9d24db370ce95b75555f7d1f8691805/raw/";
+
 export const environment = {
-  production: true
+  production: true,
+  baseUrl
 };

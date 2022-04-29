2472fa680df5c8afd781c112292f4fa7819243ff
Carlos Villamil <c.villamilc@uniandes.edu.co>
2022-04-28 22:29:10 -0500

    Se agrega faker y test para componente

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -1,17 +1,24 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { faker } from "@faker-js/faker";
 
 import { CafeListComponent } from './cafe-list.component';
+import { CafeService } from '../cafe.service';
+import { Cafe } from '../cafe';
 
 describe('CafeListComponent', () => {
   let component: CafeListComponent;
   let fixture: ComponentFixture<CafeListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CafeListComponent ]
+      declarations: [ CafeListComponent ],
+      imports: [ HttpClientModule],
+      providers: [CafeService]
     })
     .compileComponents();
   }));
@@ -19,10 +26,33 @@ describe('CafeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListComponent);
     component = fixture.componentInstance;
+
+    for (let i = 0;i<3;i++){
+      let cafe =   new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.image.imageUrl(),
+      );
+      component.cafes.push(cafe);
+    }
+    //console.log(component.cafes);
     fixture.detectChanges();
+    debug = fixture.debugElement;
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should have an 3 rows + head ", () => {
+    let tableRows = fixture.nativeElement.querySelectorAll('tr');
+    expect(tableRows.length).toBe(4);
+  });
+
 });
+
--- src/app/cafe/cafe.service.spec.ts
+++ src/app/cafe/cafe.service.spec.ts
@@ -1,11 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CafeService } from './cafe.service';
 
 describe('Service: Cafe', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [CafeService]
     });
   });
